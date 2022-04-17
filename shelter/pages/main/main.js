b0a5c29e562cb1e19e67bf4f2ac269dee37fd443
KristiBo <kristina.boroh@gmail.com>
2022-04-18 00:48:34 +0300

    feat: add some styles for mobile version, make burger-menu

--- shelter/pages/main/main.js
+++ shelter/pages/main/main.js
@@ -1,1 +1,17 @@
 console.log(`100/100, все требования выполнены.`)
+
+const iconBurger = document.querySelector('.burger');
+const menuBurger = document.querySelector('.nav');
+const menuLinks = document.querySelectorAll('.nav-link');
+
+iconBurger.addEventListener('click', function openMenu() {
+  document.body.classList.toggle('lock');
+  iconBurger.classList.toggle('open');
+  menuBurger.classList.toggle('open');
+});
+
+menuBurger.addEventListener('click', function linkOpen() {
+  document.body.classList.remove('lock');
+  iconBurger.classList.remove('open');
+  menuBurger.classList.remove('open');
+});
