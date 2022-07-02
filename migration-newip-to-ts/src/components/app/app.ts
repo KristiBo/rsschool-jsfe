3ba72d70f03ec2187cbc1d32bdbb861c9fcf415e
KristiBo <kristina.boroh@gmail.com>
2022-07-02 20:30:00 +0300

    feat: migrate app and index files from js to ts

---
+++ migration-newip-to-ts/src/components/app/app.ts
@@ -0,0 +1,21 @@
+import AppController from '../controller/controller';
+import { AppView } from '../view/appView';
+import { EverythingInt, SourcesInt } from '../../types/types';
+
+class App {
+    controller: InstanceType<typeof AppController>;
+    view: InstanceType<typeof AppView>;
+    constructor() {
+        this.controller = new AppController();
+        this.view = new AppView();
+    }
+
+    start(): void {
+        (document.querySelector('.sources') as HTMLElement).addEventListener('click', (e) =>
+            this.controller.getNews(e, (data: EverythingInt | SourcesInt) => this.view.drawNews(data))
+        );
+        this.controller.getSources((data: EverythingInt | SourcesInt) => this.view.drawSources(data));
+    }
+}
+
+export default App;
