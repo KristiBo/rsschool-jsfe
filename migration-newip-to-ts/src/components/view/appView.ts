9ed63baebee644ce6420d2a6544d6ff32c5853c5
KristiBo <kristina.boroh@gmail.com>
2022-07-02 10:58:34 +0300

    feat: migrate appView file from js to ts

--- migration-newip-to-ts/src/components/view/appView.ts
+++ migration-newip-to-ts/src/components/view/appView.ts
@@ -1,18 +1,21 @@
 import News from './news/news';
 import Sources from './sources/sources';
+import { EverythingInt, SourcesInt } from '../../types/types';
 
 export class AppView {
+    news: InstanceType<typeof News>;
+    sources: InstanceType<typeof Sources>;
     constructor() {
         this.news = new News();
         this.sources = new Sources();
     }
 
-    drawNews(data) {
+    drawNews(data: EverythingInt): void {
         const values = data?.articles ? data?.articles : [];
         this.news.draw(values);
     }
 
-    drawSources(data) {
+    drawSources(data: SourcesInt): void {
         const values = data?.sources ? data?.sources : [];
         this.sources.draw(values);
     }
